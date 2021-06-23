27e92c1cf66ad306d9977e0773c7ccdaf7a6afb2
Jon Sweeney <jonsweeney26@gmail.com>
2021-06-23 07:04:41 -0700

    Initial Commit

---
+++ InstaDog/InstaDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaDogApp.swift
+//  InstaDog
+//
+//  Created by Jonathan Sweeney on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct InstaDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
